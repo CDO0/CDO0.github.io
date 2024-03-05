6e2a55aa70cb91adbd9d758b66a15496441b0acc
Corentin <corentin.do@hotmail.fr>
2024-03-05 18:40:52 +0100

    path adjustment

--- src/js/preload_page.js
+++ src/js/preload_page.js
@@ -107,19 +107,19 @@ assetText.setOrigin(0.5, 0.5);
 
 
 //menu
-    this.load.image("fond_menu", "/src/assets/fond_menu.png");
-    this.load.image("imageLogo", "/src/assets/logo.png");
-    this.load.image("imageBoutonPlay", "/src/assets/jouerBtn.png");
-    this.load.image("imageCommandes", "src/assets/commandes.png");
-    this.load.image("gouteSolo", "/src/assets/gouteSolo.png");
+    this.load.image("fond_menu", "./src/assets/fond_menu.png");
+    this.load.image("imageLogo", "./src/assets/logo.png");
+    this.load.image("imageBoutonPlay", "./src/assets/jouerBtn.png");
+    this.load.image("imageCommandes", "./src/assets/commandes.png");
+    this.load.image("gouteSolo", "./src/assets/gouteSolo.png");
 
 
 
 
 //extra ressources pour esthétique 
-this.load.image("image", "/src/assets/image.png");
+this.load.image("image", "./src/assets/image.png");
     for (var i = 0; i < 500; i++) {
-        this.load.image("image"+i, "/src/assets/image.png");
+        this.load.image("image"+i, "./src/assets/image.png");
     }
 
 
@@ -127,24 +127,24 @@ this.load.image("image", "/src/assets/image.png");
 //main_scene
      
     // chargement tuiles de jeu
-    this.load.image("Phaser_portofolio", "/src/assets/tuilesJeu.png");
+    this.load.image("Phaser_portofolio", "./src/assets/tuilesJeu.png");
   
     // chargement de la carte
-    this.load.tilemapTiledJSON("carte", "/src/assets/map2.json");
+    this.load.tilemapTiledJSON("carte", "./src/assets/map2.json");
     
-    this.load.image("ressource", "/src/assets/ressource.png");
+    this.load.image("ressource", "./src/assets/ressource.png");
     for (var i = 0; i < 500; i++) {
-        this.load.image("ressource"+i, "/src/assets/ressource.png");
+        this.load.image("ressource"+i, "./src/assets/ressource.png");
     }
 
 
     //chargement des plateformes movibles
-    this.load.image("plateforme_roche", "/src/assets/plat_roche.png");
-    this.load.image("plateforme_grotte", "/src/assets/pilier.png");
+    this.load.image("plateforme_roche", "./src/assets/plat_roche.png");
+    this.load.image("plateforme_grotte", "./src/assets/pilier.png");
     
    
     //chargement du spritesheet point
-    this.load.spritesheet("img_point", "/src/assets/img_point.png", {
+    this.load.spritesheet("img_point", "./src/assets/img_point.png", {
        frameWidth: 32,
        frameHeight: 32
      });
@@ -153,9 +153,9 @@ this.load.image("image", "/src/assets/image.png");
 
 
 //extra ressources pour esthétique 
-     this.load.image("grass", "/src/assets/grass.png");
+     this.load.image("grass", "./src/assets/grass.png");
     for (var i = 0; i < 500; i++) {
-        this.load.image("grass"+i, "/src/assets/grass.png");
+        this.load.image("grass"+i, "./src/assets/grass.png");
     }
 
 
@@ -163,50 +163,50 @@ this.load.image("image", "/src/assets/image.png");
 
    
      // cgargement images popup
-     this.load.image("backbox_background", "/src/assets/boxBackground.png");
-     this.load.image("backbox_formation", "/src/assets/boxFormation.png");
-     this.load.image("backbox_projets", "/src/assets/boxProjets.png");
-     this.load.image("backbox_cePortfolio", "/src/assets/boxThisWT.png");
-     this.load.image("backbox_competences", "/src/assets/boxCompetences.png");
+     this.load.image("backbox_background", "./src/assets/boxBackground.png");
+     this.load.image("backbox_formation", "./src/assets/boxFormation.png");
+     this.load.image("backbox_projets", "./src/assets/boxProjets.png");
+     this.load.image("backbox_cePortfolio", "./src/assets/boxThisWT.png");
+     this.load.image("backbox_competences", "./src/assets/boxCompetences.png");
     
-    this.load.image("boxBienvenue", "/src/assets/bienvenue.png");
-    this.load.image("rappelCommandes", "/src/assets/rappelCommandes.png");
-    this.load.image("rappelCommandesTouch", "/src/assets/rappelCommandesTouch.png");
-    this.load.image("boxBienvenueTouch", "/src/assets/bienvenueTouch.png");
-    this.load.image("BtnBienvenue", "/src/assets/btnBienvenue.png");
+    this.load.image("boxBienvenue", "./src/assets/bienvenue.png");
+    this.load.image("rappelCommandes", "./src/assets/rappelCommandes.png");
+    this.load.image("rappelCommandesTouch", "./src/assets/rappelCommandesTouch.png");
+    this.load.image("boxBienvenueTouch", "./src/assets/bienvenueTouch.png");
+    this.load.image("BtnBienvenue", "./src/assets/btnBienvenue.png");
 
 //extra ressources pour esthétique 
-     this.load.image("image", "/src/assets/image.png");
+     this.load.image("image", "./src/assets/image.png");
     for (var i = 0; i < 500; i++) {
-        this.load.image("image"+i, "/src/assets/image.png");
+        this.load.image("image"+i, "./src/assets/image.png");
     }
 
 
 
     // chargement des leviers
-    this.load.image("img_levier", "/src/assets/levier_champBig.png");
-    this.load.image("img_levier_grotte", "/src/assets/levier_champSmall.png");
+    this.load.image("img_levier", "./src/assets/levier_champBig.png");
+    this.load.image("img_levier_grotte", "./src/assets/levier_champSmall.png");
     
     // chargement du personnage
-      this.load.spritesheet("img_perso", "/src/assets/goute.png", {
+      this.load.spritesheet("img_perso", "./src/assets/goute.png", {
         frameWidth: 32,
         frameHeight: 26
       });
    
       //chargement des portes
-      this.load.image("img_porte_menu", "/src/assets/menu_door.png");
-      this.load.spritesheet("portal","/src/assets/portal.png", {
+      this.load.image("img_porte_menu", "./src/assets/menu_door.png");
+      this.load.spritesheet("portal","./src/assets/portal.png", {
        frameWidth: 32,
        frameHeight: 32
       });
    
-      this.load.spritesheet("wave", "/src/assets/wave.png", {
+      this.load.spritesheet("wave", "./src/assets/wave.png", {
         frameWidth: 32,
         frameHeight: 32
       });
    
       //chargement points excalamation + activateur 
-      this.load.image("pointExc", "/src/assets/pointExc.png");
+      this.load.image("pointExc", "./src/assets/pointExc.png");
       
       
 
@@ -215,15 +215,15 @@ this.load.image("image", "/src/assets/image.png");
      
       //extra ressources pour esthétique 
       for (var i = 0; i < 500; i++) {
-          this.load.image("ressource"+i, "/src/assets/ressource.png");
+          this.load.image("ressource"+i, "./src/assets/ressource.png");
       }
       
       
 
-      this.load.image("btn_fleche_gauche", "/src/assets/fleche_gauche.png");
-     this.load.image("btn_fleche_droite", "/src/assets/fleche_droite.png");
-     this.load.image("btn_fleche_haut", "/src/assets/fleche_haut.png");
-     this.load.image("btn_espace", "/src/assets/espace.png");
+      this.load.image("btn_fleche_gauche", "./src/assets/fleche_gauche.png");
+     this.load.image("btn_fleche_droite", "./src/assets/fleche_droite.png");
+     this.load.image("btn_fleche_haut", "./src/assets/fleche_haut.png");
+     this.load.image("btn_espace", "./src/assets/espace.png");
 
 
 
@@ -235,16 +235,16 @@ this.load.image("image", "/src/assets/image.png");
 // next_level
   // chargement du tilemap propre à la scene
   // tuiles de jeu sont déja chargées dans main_scene
-  this.load.tilemapTiledJSON("carte_next_level", "/src/assets/next_level.json");
+  this.load.tilemapTiledJSON("carte_next_level", "./src/assets/next_level.json");
   
   //chargement image pour lien
-  this.load.image("lien", "/src/assets/cv_mini.png");
-  this.load.image("contact", "/src/assets/contactez.png");
+  this.load.image("lien", "./src/assets/cv_mini.png");
+  this.load.image("contact", "./src/assets/contactez.png");
 
 
   //chargement images "wanted"
-  this.load.image("wantedAlternance", "/src/assets/wantedAlternance.png");
-  this.load.image("boxCliquables", "/src/assets/boxCliquables.png");
+  this.load.image("wantedAlternance", "./src/assets/wantedAlternance.png");
+  this.load.image("boxCliquables", "./src/assets/boxCliquables.png");
 
 
 
